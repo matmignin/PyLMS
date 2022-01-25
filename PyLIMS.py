97ac7c76ce25efc38ce274af786b24ccd47c641a
MatMignin <mat@mignin.com>
2022-01-25 15:01:20 -0500

    Initial Commit

---
+++ PyLIMS.py
@@ -0,0 +1,7 @@
+from ahk import AHK
+
+ahk = AHK()
+
+ahk_script = 'Run Vquest.ahk'
+ahk.run_script(ahk_script, blocking=False)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# PyLMS
+Automation Tools for working with Nugenesis LMS and related functions. 
