3b492d846e804d26fb03c281cec24ad6b1bf9043
MatMignin <mat@mignin.com>
2022-01-25 15:05:20 -0500

    Use direct path to AHK sript for now.

--- PyLIMS.py
+++ PyLIMS.py
@@ -2,6 +2,6 @@ from ahk import AHK
 
 ahk = AHK()
 
-ahk_script = 'Run Vquest.ahk'
+ahk_script = 'Run C:\\Users\\mmignin\\Documents\\VQuest\\Vquest.ahk'
 ahk.run_script(ahk_script, blocking=False)
 
