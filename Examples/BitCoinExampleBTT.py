9eff4aa29a1310328040dd264e61ca424a2fb71b
matmignin <mat@mignin.com>
2022-03-04 00:45:44 -0500

    Add reading codes script and json file

---
+++ Examples/BitCoinExampleBTT.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urllib2,json,sys
+
+coin_ticker = "BTC" if "BTC"[0] != "{" else "BTC"
+fiat_ticker = "USD" if "USD"[0] != "{" else "USD"
+fiat_symbol = "$" if "$"[0] != "{" else  "$"
+num_format = "{}" if "{}"[2:8] != "format" else  "{}"
+mod_percent = float("0") if "0"[0] != "{" else float(0)
+output_type = "no" if "no"[0] != "{" else  "mktcap"
+api_type = "live" if "live"[0] != "{" else  "live"
+extraOptions = "False" if "False"[0] != "{" else  "&limit=1&aggregate=1&toTs=1514376000"
+offline_cache = "false" if "false"[0] != "{" else "false"
+percentageRound = int("0") if "0"[0] != "{" else int(0)
+literalRound = int("0") if "0"[0] != "{" else int(0)
+
+try:
+    if (api_type == "live"):
+
+        url = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms={}".format(coin_ticker, fiat_ticker)
+
+        data = urllib2.urlopen(url)
+        obj=json.load(data)
+
+        raw_current = float(obj["RAW"][coin_ticker][fiat_ticker]["PRICE"])
+        raw_opening = float(obj["RAW"][coin_ticker][fiat_ticker]["OPEN24HOUR"])
+        raw_high = float(obj["RAW"][coin_ticker][fiat_ticker]["HIGHDAY"])
+        raw_low = float(obj["RAW"][coin_ticker][fiat_ticker]["LOWDAY"])
+        raw_mktcap = float(obj["RAW"][coin_ticker][fiat_ticker]["MKTCAP"])
+        current = num_format.format(raw_current)
+        opening = num_format.format(raw_opening)
+        high = num_format.format(raw_high)
+        low = num_format.format(raw_low)
+
+        if (raw_mktcap > 1000000):
+            mktcap = str("{:,."+str(literalRound)+"f}").format(raw_mktcap / 1000000) + " M"
+        else:
+            mktcap = str("{:,."+str(literalRound)+"f}").format(raw_mktcap)
+
+        if (raw_current > raw_opening):
+            trend = "▲"
+        else:
+            trend = "▼"
+
+        if (output_type is "no"):
+            output = fiat_symbol + current
+        elif (output_type is "simple"):
+            output = fiat_symbol + current + " " + trend
+        elif (output_type is "mktcap"):
+            output = fiat_symbol + current + " (" + fiat_symbol + mktcap + ")"
+        elif (output_type is "absolute"):
+            output = fiat_symbol + current + " (L: " + fiat_symbol + low + " H: " + fiat_symbol + high + ")"
+        elif (output_type is "relative"):
+            output = fiat_symbol + current + " (L: -" + fiat_symbol + str(round(raw_current - raw_low, literalRound)) + " H: +" + fiat_symbol + str(round(raw_high - raw_current,literalRound)) + ")"
+        elif (output_type is "current-percentage"):
+            output = fiat_symbol + current + " (" + str(round(((raw_current - raw_opening) / raw_current) * 100, percentageRound)) + "%)"
+        elif (output_type is "range-percentage"):
+            output = fiat_symbol + current + " (L: -" + str(round (((raw_current - raw_low) / raw_current) * 100, percentageRound)) + "% H: +" + str(round (((raw_high - raw_current) / raw_current) * 100, percentageRound)) + "%)"
+        elif (output_type is "user-percentage"):
+            output = fiat_symbol + current + " (L: " + fiat_symbol + str(round(raw_current - (raw_current * mod_percent), literalRound)) + " H: " + fiat_symbol + str(round(raw_current + (raw_current * mod_percent), literalRound)) + ")"
+
+        if (offline_cache is "true"):
+            tmp_file = open("/tmp/"+coin_ticker+"-"+fiat_ticker+"-"+output_type+".txt", "w")
+            tmp_file.write(output)
+            tmp_file.close()
+
+        print(output)
+
+    elif (api_type == "historical"):
+        url = "https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}" + extraOptions
+        url = url.format(coin_ticker, fiat_ticker)
+
+        data = urllib2.urlopen(url)
+        obj=json.load(data)
+
+        raw_high = float(obj["Data"][1]["high"])
+        high = num_format.format(raw_high)
+
+        output = fiat_symbol + high
+
+        if (offline_cache is "true"):
+            tmp_file = open("/tmp/"+coin_ticker+"-"+fiat_ticker+"-"+output_type+".txt", "w")
+            tmp_file.write(output)
+            tmp_file.close()
+
+        print(output)
+except urllib2.URLError, e:
+    try:
+        tmp_file = open("/tmp/"+coin_ticker+"-"+fiat_ticker+"-"+output_type+".txt", "r")
+        print 'CACHED ' + tmp_file.read()
+    except IOError, e:
+        print('Unable to get data from API & no cache available')
+except ValueError, e:
+    print('There was an error formatting the output: %s' % e)
+# Please submit any issues https://github.com/chrislennon/Crypto-Touchbar-App/issues with the above script
--- Examples/Clip_Products.py
+++ Examples/Clip_Products.py
@@ -1,71 +1,71 @@
 
-import re, pyperclip
+import re, pyperclip, os, json
 
-example = '''
-Vollara # 101543
-Final label copy # 000K5190000
-Quote # 78762
-Rev to H581 (several rinky-dink changes – see Zoho)				02/05/22
-Capsule size: #0 clear vegetarian
-Please refer to the VQ Label Review Checklist as a guideline for an acceptable label layout.
+# def get_codes():
+data = json.load('codes.json')
 
-This Supplement Facts has been updated to comply with the new FDA regulations, “Food Labeling: Revision of the Nutrition and Supplement Facts Labels”, which took effect on July 26, 2016.
 
-EFL Sport Re:Fuel
-Dietary Supplement
-
-	Supplement Facts
-	Serving Size: 2 Capsules
-Servings Per Container: Please complete
-		Amount Per Serving	% Daily Value
-	Vitamin A (100% as natural beta-carotene)	162 mcg	18%
-	Vitamin C	20 mg	22%
-	Vitamin D [as (D3) cholecalciferol] 	20 mcg (800 IU)	100%
-	Vitamin E (as natural mixed tocopherols with tocotrienols)	6.7 mg	45%
-	Thiamin (as thiamin HCl)	0.5
-		Riboflavin	0.56 mg	43%
-	Niacin (as niacinamide)	6.6 mg	41%
-	Vitamin B6 (as pyridoxal-5-phosphate)	0.66 mg	39%
-	Folate (as calcium L-5-methyltetrahydrofolate)	222 mcg DFE	56%
-	Vitamin B12 (as methylcobalamin)	2 mcg	83%
-	Biotin 	100 mcg	333%
-	Pantothenic acid (as D-calcium pantothenate)	3.3 mg	66%
-	Calcium 	50 mg	4%
-	Iron (as Ferrochel® ferrous bisglycinate chelate)	0.76 mg	4%
-	Iodine (as potassium iodide)	50 mcg	33%
-	Magnesium 	25 mg	6%
-	Zinc 	3 mg	27%
-	Selenium (as Albion® selenium glycinate complex)	5 mcg	9%
-	Copper 	0.4 mg	44%
-	Manganese 	0.26 mg	11%
-	Chromium (as TRAACS® chromium nicotinate glycinate chelate)	70 mcg	200%
-	Molybdenum (as molybdenum amino acid chelate)	5 mcg	11%
-	Essentials for Life:
-TRAACS® calcium bisglycinate chelate buffered with calcium carbonate, TRAACS® magnesium bisglycinate chelate buffered with magnesium oxide, TRAACS® zinc bisglycinate chelate, TRAACS® magnesium glycinate glutamine chelate, TRAACS® copper bisglycinate chelate, TRAACS® manganese bisglycinate chelate, vanadium amino acid chelate, Bororganic® boron glycine complex	369 mg	*
-	Proprietary Whole Food Vitamin and Antioxidant Blend:
-Acerola fruit extract, garlic, kelp, rutin, broccoli, brussels sprouts, cabbage, spinach leaf, beet root, pomegranate fruit extract, chlorella algae, cranberry extract, Jerusalem artichoke root inulin, quercetin dihydrate, alfalfa leaf, blueberry fruit, carrot root, ginger root, grape seed extract, green pepper fruit, olive leaf extract, raspberry juice powder, shiitake mushroom extract, spirulina, sweet potato tuber, turmeric root extract, olive fruit extract, pine bark extract, resveratrol (from Polygonum cuspidatum root extract)	265 mg	*
-	Proprietary Digestive Enzyme Blend:
-Acid protease, fungal protease, glucoamylase, invertase, malt diastase, lactase, cellulase, lipase, fungal amylase, peptidase 	140 mg	*
-	Proprietary Probiotic Blend: (2 billion CFU)†
-Lactobacillus acidophilus, Bifidobacterium lactis, Streptococcus thermophilus, Bifidobacterium breve, Lactobacillus rhamnosus, Lactobacillus paracasei, Lactobacillus plantarum, Bifidobacterium bifidum, Bifidobacterium longum, Bifidobacterium adolescentis in a base of fructo-oligosaccharides from chicory root inulin	100 mg	*
-	Vitamin K2 (as menaquinone-7 from soy)	20 mcg	*
-	* Daily value not established.
-Other ingredients: Hypromellose, vegetable stearic acid, ascorbic acid, rice flour, and silica.
-
+Clipboard = '''
+2/28/22
+F
+Skinny Coffee 360g Bag
+J984
+111-0330
+Appitivi llc
+0334A2B
+2/28/22
+F
+Skinny Coffee 360g Bag
+J984
+111-0331
+Appitivi llc
+0334A2C
+2/28/22
+F
+Blue Raspberry 4g Stick
+K656
+106-1272
+Branded Acquico
+0225L1
+2/28/22
+F
+Electrolyte Lemonade Power 378g
+K607
+201-0027
+Branded Acquico
+0106A2
+2/28/22
+F
 '''
+find_product = re.compile(r'((?<=\w{3})?(?P<product>[abdefghijkl]\d{3}))(?=\w{4})?-',re.IGNORECASE|re.DOTALL)
+# found_product = find_product.search(Clipboard)
 
-find_product = re.compile(r'((?<=\w{3})?(?P<product>[abdefghijkl]\d{3}))(?=\w{4})?',re.IGNORECASE|re.DOTALL)
+find_batch = re.compile(r'(?<!Ct#)\d{3}-\d{4}\b',re.IGNORECASE|re.DOTALL)
+# found_batch = find_batch.search(Clipboard)
+
 find_name = re.compile(r'((?:2016\.\n*(?P<name>[\w \S]*))(?!Dietary Supplement))',re.IGNORECASE)
-# example = pyperclip.paste()
-found_product = find_product.search(example)
-found_name = find_name.search(example)
+
+find_lot = re.compile(r'(?P<Lot>\b\d{4}\w\d\w?|\bBulk\b|G\d{7}\w?\b|VC\d{6}[ABCDEFGH]?|V[A-Z]\d{5}[A-Z]\d?|\d{5}\[A-Z]{3}\d)',re.IGNORECASE|re.DOTALL)
+find_coated = re.compile(r'(\d{4}\w\d\w?.|\bBulk\b|G\d{7}\w?\b|VC\d{6}[ABCDEFGH]?|V[A-Z]\d{5}[A-Z]\d?|\d{5}\[A-Z]{3}\d\s|coated: |ct#?|ct\s?|coated\s?)(?P<Coated>\d{3}-\d{4})',re.IGNORECASE|re.DOTALL)
+# Clipboard = pyperclip.paste()
+# found_name = find_name.search(Clipboard)
 # print(found_name.group(1))
 
-print(found_product.group('product'))
-print(found_name.group('name'))
-print(re.findall(find_product,example))
-# print(re.finditer(found_product.group(1)))
-print(re.findall(found_name.group('name'),example))
+# print(found_product.group('product'))
+try:
+	Batch = re.findall(find_batch,Clipboard)
+	print(Batch[3])
+	# print('----')
+	# print(os.environ.get('VAR1'))
+	os.environ['VAR_Batch1'] = Batch[1]
+	print(os.environ['VAR_Batch1'])
+except:
+	print('')
+# print(found_product[1])
+# print(re.findall(find_product,Clipboard))
+# print(re.findall(find_batch,Clipboard)[1])
+# print(re.finditer(found_batch[2],Clipboard))
+# print(re.findall(found_name.group('name'),Clipboard))
 
 
 
