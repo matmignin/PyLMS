53f288946494ebab2e101546b358cdff88890c85
matmignin <mat@mignin.com>
2022-03-08 00:43:32 -0500

    Switch to using .git

--- read_product.py
+++ read_product.py
@@ -1,11 +1,29 @@
 import json
 
-with open('/Volumes/SHARED/PyLMS/codes.json') as f:
-	product = json.load(f)['products'][0]['product']
+# with open('/Volumes/SHARED/PyLMS/codes.json') as f:
+f = open('/Volumes/SHARED/PyLMS/codes.json')
+code = json.load(f)['products'][0]
+product = code.get('product')
+batch = code['batches'][0].get('batch')
+lot = code['batches'][0].get('lot')[0]
+coated = code['batches'][0].get('coated')
+
+
 try:
+	# print(product['batches'][0]['batch'] + " " + product['batches'][0]['lot'][0])
+
 	print(product)
+	print(batch)
+	print(lot)
+	print(coated)
 except:
 	print('')
+
+
+
+
+
+
 
 
 
