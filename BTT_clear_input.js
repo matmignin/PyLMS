db9ceb27ca45b55f6632afb9b9a9a9427cd1c197
matmignin <mat@mignin.com>
2022-03-15 01:09:04 -0400

    Add clear code parameter

---
+++ BTT_clear_input.js
@@ -0,0 +1,22 @@
+(async () => {
+
+let input = 'product'
+
+let shellScript = '/Users/matbook/.pyenv/versions/3.7.3/bin/python3.7 /Users/matbook/Desktop/PyLMS/read_codes.py' + ' ' + input + ' clear'
+
+let shellScriptWrapper = {
+    script: shellScript, // mandatory
+    launchPath: '/bin/bash', //optional - default is /bin/bash
+    parameters: '-c', // optional - default is -c. If you use multiple parameters please separate them by ;; e.g. -c;;date
+    environmentVariables: '' //optional e.g. VAR1=/test/;VAR2=/test2/;
+};
+
+// this will execute the Apple Script and store the result in the result variable.
+let result = await runShellScript(shellScriptWrapper);
+
+// do whatever you want with the result
+
+let string = await callBTT('set_string_variable', {variable_name: input, to: result});
+
+returnToBTT(result);
+})();
--- read_codes.py
+++ read_codes.py
@@ -1,15 +1,27 @@
 #!/Users/matbook/.pyenv/versions/3.8.3/envs/VQenv/bin/python
 import json, sys
 
-code = sys.argv[1]
+a_code = sys.argv[1]
+try:
+	clear_code = sys.argv[2]
+except:
+	clear_code = None
 
 
-with open('/Users/matbook/Desktop/PyLMS/codes.json') as f:
-	code = json.load(f)[code]
+with open('/Users/matbook/Desktop/PyLMS/codes.json', 'r+') as f:
+	data = json.load(f)
+	code = data[a_code]
+if clear_code:
+	with open('/Users/matbook/Desktop/PyLMS/codes.json', 'w') as nf:
+		data[a_code] = None
+		nf.seek(0)
+		json.dump(data, nf, indent=4)
 if code == None:
 	print('')
 else:
 	print(code)
+
+
 
 
 
