8cb677057b560805e25e70a5cf071795ee7c8527
mat@mignin.com <mat@mignin.com>
2022-03-14 23:06:53 -0400

    Add arguments to uninfy read_scripts

--- read_codes.py
+++ read_codes.py
@@ -1,22 +1,15 @@
 #!/Users/matbook/.pyenv/versions/3.8.3/envs/VQenv/bin/python
-import json
+import json, sys
 
-
+code = sys.argv[1]
 
 
 with open('/Users/matbook/Desktop/PyLMS/codes.json') as f:
-	product = json.load(f)['product']
-	# batch = json.load(f)['products'][0]['batches'][0]
-	# lot = json.load(f)['products'][0]['batches'][0]['lot'][0]
-	# coated = json.load(f)['products'][0]['batches'][0]['coated']
-try:
-	print(product)
-	# print(batch)
-	# print(lot)
-	# print(coated)
-except:
+	code = json.load(f)[code]
+if code == None:
 	print('')
-
+else:
+	print(code)
 
 
 
