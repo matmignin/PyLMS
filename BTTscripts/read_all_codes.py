20aef1fbc82973f2c5daf1d6c819eeab21c1cea6
matmignin <mat@mignin.com>
2022-09-01 14:16:40 -0400

    Include AllCodes and remove SampleId matching

--- BTTscripts/read_all_codes.py
+++ BTTscripts/read_all_codes.py
@@ -10,22 +10,15 @@ except:
 	clear_code = None
 
 # pattern_product = re.compile(r'(?P<product>[abcdefghijkl]\d{3}).(?P<batch>\b\d{3}-\d{4}).(?P<lot>\b\d{4}\w\d\w?|\bBulk\b|G\d{7}\w?\b|VC\d{6}[ABCDEFGH]?|V[A-Z]\d{5}[A-Z]\d?|\d{5}\[A-Z]{3}\d)?.*?(?:ct\#)(?P<coated>\d{3}-\d{4})?',re.IGNORECASE)
-if a_code == "product":
-	pattern = re.compile(r'(?P<product>[abcdefghijkl]\d{3})',re.IGNORECASE)
-if a_code == "batch":
-	pattern = re.compile(r'(?<!ct#)(?P<batch>\d{3}-\d{4})',re.IGNORECASE)
-if a_code == "lot":
-	pattern = re.compile(r'(?P<lot>\b\d{4}\w\d\w?|\bBulk\b|G\d{7}\w?\b|VC\d{6}[ABCDEFGH]?|V[A-Z]\d{5}[A-Z]\d?|\d{5}\[A-Z]{3}\d)',re.IGNORECASE)
-if a_code == "coated":
-	pattern = re.compile(r'(?:ct\#)(?P<coated>\d{3}-\d{4})',re.IGNORECASE)
-if a_code == "sampleid":
-	pattern = re.compile(r'(?P<sampleid>(s|\$)202\d{5}-\d{3}|[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})',re.IGNORECASE)
 
 try:
-	with open(os.path.expanduser('/Volumes/~mmignin/RemoteVQ/Code.txt'), 'r') as f:
-		code = f.read()
-		match = pattern.search(code)
-	print(match.group(a_code))
+	if a_code == "product":
+		with open(os.path.expanduser('/Volumes/~mmignin/RemoteVQ/AllProducts.txt'), 'r') as f:
+			code = f.read()
+	if a_code == "batch":
+		with open(os.path.expanduser('/Volumes/~mmignin/RemoteVQ/AllBatches.txt'), 'r') as f:
+			code = f.read()
+	print(code)
 except:
 	print('')
 # with open(os.path.expanduser('~/PyLMS/codes.json'), 'r+') as f:
